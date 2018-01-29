1fe43e4da31fd279501961b157d91348b384ee03
PV-X00151013\Admin <577574461@qq.com>
2018-01-29 15:51:56 +0800

    样式修改

--- jry-web-master-jry/wap/js/conttrollers/controllerbkcq.js
+++ jry-web-master-jry/wap/js/conttrollers/controllerbkcq.js
@@ -1,22 +1,18 @@
 //暂无controller对应
 define(['js/module.js', 'jquery', 'ngdialog'], function (controllers, $, ngdialog) {
-    controllers.controller('controllerbkcq', function ($scope, resourceService, $filter, $http, $state, $rootScope, $localStorage, ngDialog, $stateParams,md5) {
-        console.log(11111)
-	         
+    controllers.controller('controllerbkcq', function ($scope, resourceService, $filter, $http, $state, $rootScope, $localStorage, ngDialog, $stateParams,md5) {	         
      	$scope.changeToggle = function(event){
-     	  	console.log(11111)
-//          $(".g-nav-ct main").slideUp();
-           if ($(event.currentTarget).siblings("main").hasClass("new")) {            	
+            if ($(event.currentTarget).siblings("main").hasClass("new")) {   
             	$(event.currentTarget).siblings("main").slideUp('slow').removeClass("new");
             	
             	$(event.currentTarget).find('i').removeClass('animation1').addClass('animation2');
-            } else{
-            	
-            	$(event.currentTarget).siblings("main").slideDown('slow').addClass("new");
-            	$(event.currentTarget).parent().siblings("div").children("main").slideUp('slow').removeClass("new");
-            	$(event.currentTarget).find('i').removeClass('animation2').addClass('animation1');
-            	$(event.currentTarget).parent().siblings("div").find('i').addClass('animation2').removeClass('animation1');	
+            } else{           	
+              	$(event.currentTarget).siblings("main").slideDown('slow').addClass("new");
+              	$(event.currentTarget).parent().siblings("div").children("main").slideUp('slow').removeClass("new");
+              	$(event.currentTarget).find('i').removeClass('animation2').addClass('animation1');
+                $(event.currentTarget).parent().siblings("div").find('i').removeClass('animation1');            	            		            		            	            	            	            		
             }
+                                                                                                                                                
         }
     })
 })
