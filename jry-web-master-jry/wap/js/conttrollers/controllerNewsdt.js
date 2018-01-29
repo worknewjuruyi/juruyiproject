4ac0c16643f479cfcf35644a44baeb54b1b17f9f
PV-X00151013\Admin <577574461@qq.com>
2018-01-29 11:46:26 +0800

    新闻详情页面 修改 （样式 controller iscroll）

--- jry-web-master-jry/wap/js/conttrollers/controllerNewsdt.js
+++ jry-web-master-jry/wap/js/conttrollers/controllerNewsdt.js
@@ -1,114 +1,15 @@
 define(['js/module.js', 'jquery', 'ngdialog'], function (controllers, $, ngdialog) {
-    controllers.controller('controllerNewsdt', function ($scope, resourceService, $filter, $http, $state, $rootScope, $localStorage, ngDialog, $stateParams,md5) {
-    
-    
-    
-    
-//	$http.get('http://192.168.17.101:8888/aboutus/newsDetails.do').  
-//  success(function(data) {
-//  	
-//      console.log(data);  
-//  }).  
-//  error(function(err) {  
-//      //错误代码  
-//  });
-	var srollsFirst = document.getElementById('slide2');
-     var myscrollFirst = new iScroll(srollsFirst,{
-       vScrollbar: false,
-       onScrollEnd: function(){
-         if((this.y == this.maxScrollY)){               
-           // 加载数据
-           setTimeout(function(){                
-             myscrollFirst.refresh();
-           },10)
-         }
-       }
-     })
-   
+    controllers.controller('controllerNewsdt', function ($scope,$sce,resourceService, $filter, $http, $state, $rootScope, $localStorage, ngDialog, $stateParams,md5) {
+          
 //  var time=new Date().getTime();
 //  var time2=new Date();
 //  console.log(time2.getFullYear().toString()+'-'+(time2.getMonth()+1).toString()+'-'+time2.getDate().toString());
 //  console.log(typeof(time2.getFullYear()))
 //  $scope.newtime=time;
-    
-    
-    
-//   resourceService.queryPost($scope, $filter('getUrl')(''), {}, { name: '新闻详情'});
-//  $scope.$on('resourceService.QUERY_POST_MYEVENT', function (event, data, type){
-//  	    console.log(type.name)
-//          switch (type.name){                
-//              case '新闻详情':
-//                  console.log(11111)
-//                  if (data.success) {
-//                      console.log(data)
-//                  } else {
-//                      
-//                  };
-//                  break;
-//          };
-//      });
 
-
-
-//   $.ajax({
-//	     type:"get",    //请求方式
-//	      async:true,    //是否异步
-//	      url:"http://192.168.17.101:8888/aboutus/newsDetails.do",
-//	      dataType:"json",    //跨域json请求一定是jsonp
-////	      xhrFields: {
-////	           withCredentials: true
-////	       },
-//           headers: {
-//               'Content-type': 'application/json'
-//           },
-////	       crossDomain: true,
-////	      jsonp: "callback",    //跨域请求的参数名，默认是callback
-//	          //jsonpCallback:"successCallback",    //自定义跨域参数值，回调函数名也是一样，默认为jQuery自动生成的字符串
-//	      data:{},    //请求参数
-//	
-//	     success: function(data) {
-//	         console.log(data)
-//	     },
-//	   
-//	   
-//	     error: function() {
-//	         //请求出错处理
-//	         console.log(111111)
-//	     }
-//	 });
-
-
-
-//          $http({  
-//             url:'aboutus/newsDetails.dos',  
-//             
-//             	method:"GET"                           
-//         }).success(function(data){
-//          	console.log(data)
-//         }).error(function(data){
-//          	console.log(data)
-//         })
-
-//          $http({
-//           method: 'POST',
-//           url: 'aboutus/newsDetails.dos',
-//           headers: {
-//               'Content-Type' : 'application/x-www-form-urlencoded'
-//           },
-//           data: { }
-//       }).then(function success(result){
-//           //数据请求成功
-//           console.log(result.data);
-//       },function error(err){
-//           //数据请求失败
-//           console.log(err);
-//       });
-
-//
 //      console.log($stateParams.artiId) 
-//      $scope.newartiId=$stateParams.artiId
-        console.log("yeyeyeyey")                                
-        resourceService.queryPost($scope, $filter('getUrl')('新闻详情'), {artiId:1}, '新闻详情');
+//      $scope.newartiId=$stateParams.artiId                               
+        resourceService.queryPost($scope, $filter('getUrl')('新闻详情'), {artiId:38}, '新闻详情');
         $scope.$on('resourceService.QUERY_POST_MYEVENT', function (event, data, type) {
          console.log(111111)
             switch(type){
@@ -119,12 +20,29 @@ define(['js/module.js', 'jquery', 'ngdialog'], function (controllers, $, ngdialo
                         $scope.newsTitle = article.title;
                         $scope.creatTime = article.createTime;
                         $scope.articleImgSrc = article.litpic;
-                        $scope.content = article.content;
+                        //调用$sce转化html输出到页面($sce必须先注入才能使用)
+                        $scope.content = $sce.trustAsHtml(article.content);
+                        //iscroll滚动                     
+                        $scope.$watch('$viewContentLoaded', function() {//$watch监听页面加载完毕才执行iscroll
+					    	var srollsFirst = document.getElementById('slide2');
+					        var myscrollFirst = new iScroll(srollsFirst,{
+					          vScrollbar: false,
+					          onScrollEnd: function(){
+					            if( (this.y == this.maxScrollY)) {	              
+					              // 加载数据
+					              setTimeout(function(){	              	
+					                myscrollFirst.refresh();
+					              },10)
+					            }
+					          }
+					        })
+				        }); 
                     }else{
                         console.log(12121212);
                     }
                 break;                                       
             };
         });
+                                                             
     })
 })
